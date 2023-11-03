a000101f8005309fd26960aa02340392c03dcc2b
Simon Thorell <kontakt@simonthorell.se>
2023-11-03 15:45:53 +0100

    Idea for card management handling

--- include/card_management.h
+++ include/card_management.h
@@ -1,6 +1,11 @@
 #ifndef CARD_MANAGEMENT_H
 #define CARD_MANAGEMENT_H
 
+enum cardAccess {
+    NO_ACCESS = 0,
+    ACCESS = 1
+};
+
 void listAllCards(void);
 void addRemoveAccess(void);
 
--- src/card_management.c
+++ src/card_management.c
@@ -1,12 +1,36 @@
-// #include "card_management.h"
+#include "card_management.h"
 #include <stdio.h>
+#include "safeinput.h"
 
 void listAllCards(void) {
     // List all RFID cards from the system's database
     printf("Listing all registered RFID cards...\n");
 }
 
+// Add or remove access for individual RFID cards
 void addRemoveAccess(void) {
-    // Add or remove access for individual RFID cards
-    printf("Add/Remove access to an RFID card.\n");
+    int cardNumber;
+    int cardAccess;
+    GetInputInt("Enter cardnumber: ", &cardNumber);
+
+    // Check if card number is in array of registered cards, assign access value to cardAccess else cardAccess = NO_ACCESS.
+    // Check if card number is in array of registered cards, else last card ID + 1
+
+    // if (cardNumber-->access == ACCESS) {
+    //     printf("Card currently %d has access\n", cardnumber);
+    // } else if (cardNumber-->access == NO_ACCESS) {
+    //     printf("Card currently%d has no access\n", cardnumber);
+    // } else {
+    //     printf("New card with ID %d has been registered\n", cardnumber);
+    // }
+    
+    GetInputInt("Enter new card access (0 = no access, 1 = access): ", &cardAccess);
+    
+    if (cardAccess == NO_ACCESS) {
+        printf("Removing access for card %d\n", cardNumber);
+    } else if (cardAccess == ACCESS) {
+        printf("Adding access for card %d\n", cardNumber);
+    } else {
+        printf("Invalid access value: %d\n", cardAccess);
+    }
 }
